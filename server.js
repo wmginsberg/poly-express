0f817604b9e7ee22123e03ced1f17063da2c8d26
Wendy Ginsberg <wmginsberg@gmail.com>
2016-11-02 19:25:24 -0700

    routing is working with res.sendFile

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 // Node.js notation for importing packages
 var express = require('express');
+var path = require('path');
 
 // Spin up a server
 var app = express();
 
+
 // Serve static files from the /client/ directory
-app.use(express.static(__dirname + '/client/'));
+// app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(__dirname));
+
 
 // Send 'Hello World' to the main page
-app.get('/', function (req, res) {
-  res.render("index.html");
+
+
+app.get('/', function(req, res){
+    res.sendFile("index.html", {root: '.'});
+});
+
+app.get('/view1', function(req, res){
+    res.sendFile("index.html", {root: '.'});
+});
+
+app.get('/view2', function(req, res){
+    res.sendFile("index.html", {root: '.'});
+});
+
+app.get('/view3', function(req, res){
+    res.sendFile("index.html", {root: '.'});
 });
 
 // Tell the app to listen for requests on port 3000
